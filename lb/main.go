f50cbc38ec5b23610b0ad193e34d0183e65a03d9
Inderjot Singh <inderjotsingh141@gmail.com>
2024-12-12 22:40:44 +0530

    completed minimal load balander implementation

--- lb/main.go
+++ lb/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 const (
-	HEALTH_DURATION_SEC = 60
+	HEALTH_DURATION_SEC = 10
 )
 
 type NewConnectionInput struct {
@@ -15,7 +18,7 @@ type NewConnectionInput struct {
 }
 
 type RemoveConnectionInput struct {
-	id string `json:"id" binding:"required"`
+	ID string `json:"id" binding:"required"`
 }
 
 func allConnections(c *gin.Context, lb *LoadBalancer) {
@@ -44,8 +47,69 @@ func removeConnection(c *gin.Context, lb *LoadBalancer) {
 		return
 	}
 
-	lb.removeConnection(input.id)
-	c.JSON(http.StatusOK, gin.H{"message": "Success", "url": input.id})
+	lb.removeConnection(input.ID)
+	c.JSON(http.StatusOK, gin.H{"message": "Success", "url": input.ID})
+}
+
+func makeRequestToBackend(c *gin.Context, lb *LoadBalancer) {
+
+	method := c.Request.Method
+	path := c.Request.URL.Path
+	query := c.Request.URL.RawQuery
+	headers := c.Request.Header
+
+	var bodyBytes []byte
+	if c.Request.Body != nil {
+		bodyBytes, _ = io.ReadAll(c.Request.Body)
+	}
+
+	connection, err := lb.getNextTarget()
+
+	if err != nil {
+		fmt.Print(err)
+		c.JSON(500, gin.H{"error": "Error finding target"})
+		return
+	}
+
+	backendURL := connection.URL + path
+
+	if query != "" {
+		backendURL += "?" + query
+	}
+
+	fmt.Printf("Path %s \n", path)
+	fmt.Printf("Formatted Backend Url %s \n", backendURL)
+
+	req, err := http.NewRequest(method, backendURL, bytes.NewReader(bodyBytes))
+	if err != nil {
+		fmt.Print(err)
+		c.JSON(500, gin.H{"error": "Failed to create request"})
+		return
+	}
+
+	for key, values := range headers {
+		for _, value := range values {
+			req.Header.Add(key, value)
+		}
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to forward request to backend"})
+		return
+	}
+	defer resp.Body.Close()
+
+	respBody, _ := io.ReadAll(resp.Body)
+
+	for key, values := range resp.Header {
+		for _, value := range values {
+			c.Header(key, value)
+		}
+	}
+	c.Data(resp.StatusCode, resp.Header.Get("Content-Type"), respBody)
+
 }
 
 func main() {
@@ -65,6 +129,10 @@ func main() {
 
 	router.POST("/lb/remove-connection", func(ctx *gin.Context) {
 		removeConnection(ctx, lb)
+	})
+
+	router.NoRoute(func(c *gin.Context) {
+		makeRequestToBackend(c, lb)
 	})
 
 	router.Run(":8080")
